3b38744ef93535c99eeb745f561e7d059a935ae0
PREME <preme@MacBookAirPREME.local>
2026-05-28 12:20:42 +0700

    feat: เพิ่มเรียกดูใบแจ้งซ่อมทั้งหมด

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -30,3 +30,55 @@ exports.createTicket = async (req, res) => {
     res.status(500).json({ error: "เกิดข้อผิดพลาดจากเซิร์ฟเวอร์" });
   }
 };
+
+exports.getAllTicket = async (req, res) => {
+  try {
+    const userRole = req.user.role;
+    const userId = req.user.id;
+
+    let sql = "";
+    let values = [];
+
+    if (userRole === "it_support" || userRole === "admin") {
+      sql = "SELECT * FROM tickets ORDER BY created_at DESC";
+      values = [];
+    } else {
+      sql =
+        "SELECT * FROM tickets WHERE requester_id = ? ORDER BY created_at DESC";
+      values = [userId];
+    }
+
+    const [tickets] = await db.query(sql, values);
+
+    res.status(200).json({
+      message: "ดึงข้อมูลสำเร็จ",
+      total_tickets: tickets.length,
+      tickets: tickets,
+    });
+  } catch (error) {
+    console.error("ระบบดึงข้อมูลแจ้งซ่อมขัดข้อง:", error);
+    res.status(500).json({ error: "เกิดข้อผิดพลาดจากเซิร์ฟเวอร์" });
+  }
+};
+
+exports.getTicketById = async (req, res) => {
+  try {
+    const ticketId = req.params.id;
+
+    const sql = "SELECT * FROM tickets WHERE id = ?";
+
+    const [tickets] = await db.query(sql, [ticketId]);
+
+    if (tickets.length === 0) {
+      return res.status(404).json({ message: "ไม่พบใบแจ้งซ่อมที่ระบุ" });
+    }
+
+    res.status(200).json({
+      message: "ดึงข้อมูลสำเร็จ",
+      ticket: tickets[0],
+    });
+  } catch (error) {
+    console.error("ระบบดึงข้อมูลรายบุคคลขัดข้อง:", error);
+    res.status(500).json({ error: "เกิดข้อผิดพลาดจากเซิร์ฟเวอร์" });
+  }
+};
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -4,5 +4,7 @@ const ticketController = require('../controllers/ticketController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', authMiddleware.verifyToken, ticketController.createTicket);
+router.get('/', authMiddleware.verifyToken, ticketController.getAllTicket);
+router.get('/:id', authMiddleware.verifyToken, ticketController.getTicketById);
 
 module.exports = router;
